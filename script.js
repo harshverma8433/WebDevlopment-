ad10b02d52a24ff5a2827b28896428158e8b58a5
harshverma8433 <93326986+harshverma8433@users.noreply.github.com>
2023-06-07 17:14:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,225 @@
+function f1(){
+    let r = document.getElementById("r1");
+     
+    r.innerHTML = `
+
+    <div class=" flex justify-center space-x-20 h-12 mb-24 pb-12  ">
+
+        <div class="flex flex-col">
+            <a href="#"><img class="h-16" src="ps5.jpeg" alt=""></a>
+            <p class="pl-2 pt-1"> PS5 </p>
+        </div>
+        <div> 
+            <a href="#"><img class="h-16" src="ps4.png" alt=""></a>
+            <p class="pl-2 pt-1"> PS4 </p>
+        </div>
+        <div>
+            <a href="#"><img class="h-16" src="psvr.png" alt=""></a>
+            <p class="pl-1 pt-1"> PS VR2</p>
+        </div>
+        <div>
+            <a href="#"><img class="h-16" src="psplus.png" alt=""></a>
+            <p class="pl-7 pt-1"> PS</p>
+        </div>
+        <div>
+            <a href="#"><img class="h-16" src="buy.png" alt=""></a>
+            <p class="pl-3 pt-1"> PS5 </p>
+        </div>
+
+        <hr>
+
+        
+
+    </div>
+    <div>
+        <hr class="pt-4 ">
+        <div class="flex space-x-4 justify-center list-disc pb-4 ">
+            <a href="#" class="hover:text-blue-500"> <li> PlayStation Indies </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li>PS4 games on PS5</1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li> Free to Play </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li>PlayStation games on PC </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li>Deals & offers </li> </a>
+        </div>
+    </div>
+`
+
+    r.classList.toggle('hide');
+    ;
+}
+
+function f2(){
+    let r = document.getElementById("r1");
+    r.innerHTML = `
+    <div class="flex justify-center space-x-20 h-12 mb-24 pb-12  ">
+
+        <div class="flex flex-col items-center">
+            <a href="#"><img class="h-16" src="ps5.jpeg" alt=""></a>
+            <p class=" pt-1"> PS5 </p>
+        </div>
+        <div> 
+            <a href="#"><img class="h-16" src="ps4.png" alt=""></a>
+            <p class="pl-2 pt-1"> PS4 </p>
+        </div>
+        <div>
+            <a href="#"><img class="h-16" src="psvr.png" alt=""></a>
+            <p class="pl-2 pt-1"> PS VR2</p>
+        </div>
+
+        <hr>
+
+        
+
+    </div>
+    <div>
+        <hr class="pt-4 ">
+        <div class="flex space-x-4 justify-center list-disc pb-4">
+            <a href="#" class="hover:text-blue-500"> <li> DualSense wireless controller </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li>PULSE 3D wireless headset</1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li> DUALSHOCK 4 wireless controller   </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li> PS5 & PS4 accessories </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li> PlayStation VR </li> </a>
+        </div>
+    </div>
+    `
+    r.classList.toggle('hide');
+    ;
+}
+function f3(){
+    let r = document.getElementById("r1");
+    r.innerHTML = `
+    <div class="flex justify-center space-x-20 h-12 mb-24 pb-12  ">
+
+        <div class="flex flex-col">
+            <a href="#"><img class="h-16" src="psplus.png" alt=""></a>
+            <p class="pl-5 pt-1"> PS5 </p>
+        </div>
+        
+
+        <hr>
+
+        
+
+    </div>
+    <div>
+        <hr class=" pt-4 ">
+        <div class="flex space-x-4 justify-center list-disc pb-4">
+            <a href="#" class="hover:text-blue-500"> <li> PS5 entertainment </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li> PS4 entertainment</1i> </a>
+        </div>
+    </div>
+    `
+    r.classList.toggle('hide');
+    ;
+}
+function f4(){
+    let r = document.getElementById("r1");
+    r.innerHTML = `
+    <div class="flex justify-center space-x-20 h-12 mb-24 pb-12  ">
+
+        <div class="flex flex-col">
+            <a href="#"><img class="h-16" src="blog.png" alt=""></a>
+            <p class="pl-5 pt-1"> PS5 </p>
+        </div>
+        <div> 
+            <a href="#"><img class="h-16" src="cal.png" alt=""></a>
+            <p class="pl-5 pt-1"> PS4 </p>
+        </div>
+        
+        <hr>
+
+        
+
+    </div>
+    <div>
+        <hr class="pt-4 ">
+        <div class="flex space-x-4 justify-center list-disc pb-4">
+            <a href="#" class="hover:text-blue-500 font-semibold text-base"> <li> Accessibility </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li>Privacy & Safety</1i> </a>
+        </div>
+    </div>
+    `
+    r.classList.toggle('hide');
+    ;
+}
+
+function f5(){
+    let r = document.getElementById("r1");
+    r.innerHTML = `
+    <div class="flex justify-center space-x-20 h-12 mb-24 pb-12  ">
+
+        <div class="flex flex-col items-center">
+            <a href="#"><img class="h-16" src="buy.png" alt=""></a>
+            <p class=" pt-1"> Digital Games and  </p>
+            <p class=" pt-1"> Services </p>
+
+        </div>
+        
+        <hr>
+
+        
+
+    </div>
+    <div>
+        <hr class="pt-4 ">
+        <div class="flex gap-x-12 justify-center pb-4">
+            <a href="#" class="hover:text-blue-500"> <li class="list-disc"> New releases </li> </ul> </a>
+            <a href="#" class="hover:text-blue-500"> <li class="list-disc"> Latest discounts </1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li class="list-disc"> Collections  </1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li class="list-disc"> Buy gift card </1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li class="list-disc"> Subscribe to PS Plus </1i> </a>
+        </div>
+    </div>
+    `
+    
+    r.classList.toggle('hide');
+    ;
+}
+function f6(){
+    let r = document.getElementById("r1");
+    r.innerHTML = `
+    <div class="flex justify-center space-x-20 h-12 mb-24 pb-12  ">
+
+        <div class="flex flex-col">
+            <a href="#"><img class="h-16" src="sup.png" alt=""></a>
+            <p class="pl-2 pt-1"> Support </p>
+        </div>
+        <div> 
+            <a href="#"><img class="h-16" src="psns.png" alt=""></a>
+            <p class="pl-2 pt-1"> PSN Status </p>
+        </div>
+        
+        <hr>
+
+        
+
+    </div>
+    <div>
+        <hr class="pt-4 ">
+        <div class="flex space-x-4 justify-center list-disc pb-4">
+            <a href="#" class="hover:text-blue-500"> <li> Account </li> </a>
+            <a href="#" class="hover:text-blue-500"> <li> Store </1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li> Subscriptions </1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li> Games </1i> </a>
+            <a href="#" class="hover:text-blue-500"> <li> Hardware </1i> </a>
+        </div>
+    </div>
+    `
+    
+    r.classList.toggle('hide');
+    ;
+}
+
+
+
+// function res(){
+//     let r = document.getElementById('navv');
+//     // if(r.style.display === "block"){
+//     //     r.style.display = "none";
+//     // }
+//     // else{
+//     //     r.style.display = "block";
+//     // }
+//     r.classList.toggle('hide');
+// }
+
+
